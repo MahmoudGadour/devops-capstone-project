4acf8e567b03dfa2a88b6b01e8017305e21e0404
MahmoudGadour <mahmoudgadour@gmail.com>
2024-12-17 19:46:28 +0400

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -118,6 +118,7 @@ def check_content_type(media_type):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -128,10 +129,10 @@ def update_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-        
     account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
+
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
@@ -145,6 +146,7 @@ def delete_accounts(account_id):
         account.delete()
     return "", status.HTTP_204_NO_CONTENT
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -156,3 +158,4 @@ def list_accounts():
     account_list = [account.serialize() for account in accounts]
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
+
